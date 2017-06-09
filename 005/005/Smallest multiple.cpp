f41f5eb1eb4ad3305177d4baf2027b339879b46a
linjiangcg <linjiangcg@qq.com>
2017-06-09 18:43:11 +0800

    problem 5
    Smallest multiple

---
+++ 005/005/Smallest multiple.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	long long int a = 2520;
+	int i;
+	for (; ;)
+	{
+		i = 1;
+		while (i <= 20)
+		{
+			if (a % i == 0)
+			{
+				i++;
+			}
+			else
+			{
+				break;
+			}
+			if (i == 21)
+			{
+				printf("%lld", a);
+
+			}
+		}
+		if (i == 21)
+		{
+			break;
+		}
+		a = a + 20;
+	}
+	return 0;
+}
